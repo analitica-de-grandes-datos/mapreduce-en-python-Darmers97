bea05ba20990acfafda74641baa778b60c5f78cf
Sergio Arango Jaramillo <47546473+Darmers97@users.noreply.github.com>
2022-06-13 20:58:05 -0500

    Update reducer.py
    first try

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+
+    value = []
+    ky = []
+
+    for line in sys.stdin:
+
+        value.append(int(line.split("\n")[1]))
+        ky.append(line.split("\n")[0])
+
+    ky_value = zip(ky, value)
+    
+    ky_value.sort(key=lambda x:x[1])
+    
+    for ky, value in ky_value:
+      sys.stdout.write("{},{}\n".format(ky, value))
