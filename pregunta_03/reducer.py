a02c33f6e1a73b7b0e6be9a71fa2ef99905c7b37
Sergio Arango Jaramillo <47546473+Darmers97@users.noreply.github.com>
2022-06-13 21:20:46 -0500

    Update reducer.py
    #4

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -8,13 +8,12 @@ if __name__ == '__main__':
     ky = []
 
     for line in sys.stdin:
-
-        value.append(line.split("\t")[1])
         ky.append(line.split("\t")[0])
+        value.append(int(line.split("\t")[1]))
 
-    ky_value = zip(ky, int(value))
-    
-    ky_value.sort(key=lambda x:x[1])
-    
-    for ky, value in ky_value:
+    ky_value = zip(ky, value)
+
+    ky_value_2 = sorted(ky_value,key=lambda x:x[1])
+
+    for ky, value in ky_value_2:
       sys.stdout.write("{},{}\n".format(ky, value))
