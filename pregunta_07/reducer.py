f5a685f1cd65360cd19107ea78b9f047a4433b26
Sergio Arango Jaramillo <47546473+Darmers97@users.noreply.github.com>
2022-06-14 21:07:18 -0500

    Update reducer.py
    second try

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     for line in sys.stdin:
         letter.append(line.split("\t")[0])
         date.append(line.split("\t")[1])
-        num.(int(line.split("\t")[0]))
+        num.append(int(line.split("\t")[2]))
 
     ky_value = zip(letter, date, num)
 
     ky_value_2 = sorted(ky_value,key=lambda x:(x[0],x[2]))
 
-    for ky, value in ky_value_2:
+    for letter, date, num in ky_value_2:
       sys.stdout.write("{} {} {}\n".format(letter, date, num))
