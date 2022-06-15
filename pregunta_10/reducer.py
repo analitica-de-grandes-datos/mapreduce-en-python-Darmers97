7273ec03325f88277415b1107d49bb582f58ba0d
Sergio Arango Jaramillo <47546473+Darmers97@users.noreply.github.com>
2022-06-14 22:13:32 -0500

    Update reducer.py
    frist try

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,28 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+from collections import OrderedDict
+if __name__ == '__main__':
+
+    dicty = {}
+
+    for line in sys.stdin:
+       num, letters = line.split('\t')
+       num = int(num)
+       letters = letters.split(',')
+
+       for x in letters:
+         
+         if x in dicty.keys():
+           dicty[x].append(num)
+         else:
+           dicty[x] = [num]
+
+    #sortedDict = OrderedDict(sorted(dicty.items(), key=lambda x: x[1]))
+
+
+    for ky in sorted(dicty.keys()):
+      nums = sorted(dicty[ky])
+      final_nums = ",".join([str(value) for value in nums])
+      sys.stdout.write("{}\t{}\n".format(ky,nums))
